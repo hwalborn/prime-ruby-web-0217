3e35075dd7f8b226e3e363582c8b939b5522afa6
Holt Walborn <hwalborn@gmail.com>
2017-01-05 17:57:55 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+def prime?(number)
+  control = 0
+  counter = 1
+  if number > 1
+    while counter <= number
+      if number % counter == 0
+        control += 1
+      end
+      counter += 1
+    end
+  else
+    return false
+  end
+  if control > 2
+    return false
+  else
+    return true
+  end
+end
+
+puts prime?(101013)
